9d8a0797d67ace8e058a7b3a7c258b05ec937520
Leon <Liangz.org@gmail.com>
2020-09-09 22:52:25 +0800

    feat: add three exams

--- ch1/echo1.go
+++ ch1/echo1.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	var s, sep string
+	/*
+	 * exam 1.3
+	 */
+	a := time.Now()
+	fmt.Println(a)
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
+	b := time.Since(a)
+	fmt.Println(b)
 	fmt.Println(s)
 }
--- ch1/echo3.go
+++ ch1/echo3.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	fmt.Println(strings.Join(os.Args[1:], " "))
 	fmt.Println(os.Args[1:])
+	fmt.Println(os.Args[0]) // exam 1.1
+	fmt.Println(len(os.Args))
+	a := time.Now()
+	for i := 0; i < len(os.Args); i++ {
+		fmt.Println(i, " ", os.Args[i]) //exam 1.2
+	}
+	b := time.Since(a)
+	fmt.Println(b)
 }
